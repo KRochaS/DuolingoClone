8831e6f7b84d5376435190b8eaa2cc7bf3223f27
KRochaS <karinerocha.s@hotmail.com>
2021-01-12 17:25:09 -0300

    Learn

---
+++ src/pages/Learn/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Wrapper, Container, Main } from './styles';
+
+import Header from '../../components/Header';
+
+
+
+const Learn: React.FC = () => {
+    return (
+        <Wrapper>
+
+            <Container>
+            <Header/>
+                <Main />
+            </Container>
+        </Wrapper>
+    );
+};
+
+export default Learn;
---
+++ src/pages/Learn/styles.ts
@@ -0,0 +1,25 @@
+
+import styled from 'styled-components/native';
+
+import Constants from 'expo-constants';
+import {Platform } from 'react-native';
+import colors from '../../styles/colors';
+
+const statusBarHeight = Platform.OS === 'android' ? Constants.statusBarHeight : 0;
+export const Wrapper = styled.SafeAreaView`
+    background: ${colors.primary};
+    flex: 1;
+    padding-top: ${statusBarHeight + 'px'}; 
+
+    
+`;
+
+export const Container = styled.View`
+    padding-left: 0px;
+    padding-right: 0px;
+
+`;
+
+export const Main = styled.View`
+
+`;
