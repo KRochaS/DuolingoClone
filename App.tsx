ca53b69d91dc2144fc863b290ff8741325f45edd
KRochaS <karinerocha.s@hotmail.com>
2021-01-12 17:25:49 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { useFonts } from 'expo-font';
+import Routes from './src/routes';
+import Header from './src/components/Header';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let [fontsLoaded] = useFonts({
+        DINRoundPro: require('./src/assets/fonts/DINRoundPro-Black.ttf'),
+    });
+
+    if(!fontsLoaded) {
+        return null;
+    }
+    return (
+        <View style={styles.container}>
+          
+            <Routes />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
 });
